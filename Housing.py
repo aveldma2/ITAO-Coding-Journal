56afdd9d977725108e81f75c7e2ad21c255dcc11
Aleida <aveldma2@nd.edu>
2021-09-20 12:40:21 -0400

    added housing class

--- Housing.py
+++ Housing.py
@@ -1,5 +1,22 @@
-class Location:
-    pass
+import random
 
-class House(Location):
-    pass
+class Location:
+    def __init__(self):
+        self._values = ['woods', 'beach', 'city']
+    def choose(self):
+        random.choice(self._values)
+    def value(self):
+        return self._current_value
+
+class City(Location):
+    def __init__(self, *args, **kwargs):
+        self._values = ['apartment', 'house']
+        print(random.choice(self._values))
+class Woods(Location):
+    def __init__(self, *args, **kwargs):
+        self._values = ['cabin', 'tent']
+        print(random.choice(self._values))
+class Beach(Location):
+    def __init__(self, *args, **kwargs):
+        self._values = ['beach house', 'hut']
+        print(random.choice(self._values))
--- journal.py
+++ journal.py
@@ -1,7 +1,11 @@
 from Children import *
+from Housing import *
 import os
 
 os.system('clear')
 
 children = Children()
+
+city = City()
+location = Location()
 
