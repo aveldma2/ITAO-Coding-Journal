f9fbc33848726aed258409ecf34458b00e58555c
Aleida <aveldma2@nd.edu>
2021-10-06 16:49:27 -0400

    added animal class and method for name

--- FinalPractice/zoo.py
+++ FinalPractice/zoo.py
@@ -1,5 +1,16 @@
 #practice for final
 
 class Animal:
-    pass
+    def __init__(self, name, species, num_legs):
+        self.name = name
+        self.species = species
+        self.num_legs = num_legs
+
+    def give_name(self):
+        return self.name
+
+
+a = Animal(name = 'Bob', species= 'dog', num_legs= 4)
+
+print(a.give_name())
 
